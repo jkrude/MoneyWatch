47723d158dc6a157932a361ddfcd841d9af02e4a
Jakob <jakob.krude@hotmail.com>
2020-01-24 19:03:49 +0100

    Removed unused methods

--- src/main/java/com/jkrude/material/Utility.java
+++ src/main/java/com/jkrude/material/Utility.java
@@ -2,78 +2,10 @@ package com.jkrude.material;
 
 import java.text.SimpleDateFormat;
 import java.time.Instant;
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 import javafx.util.StringConverter;
 
 public abstract class Utility {
-
-
-  public static long parseToCents(String input) {
-    input = input.replace(",", ".");
-
-    float cents = Float.parseFloat(input);
-    cents *= 100;
-    return (long) cents;
-  }
-
-  public static List<Integer> mapDateToInteger(List<String> strings) {
-    // uses the format dd.mm.yy
-      if (strings == null || strings.contains(null)) {
-          throw new NullPointerException();
-      }
-
-    List<Integer> res = new ArrayList<>();
-    for (String string : strings) {
-      String[] parts = string.split("[.]");
-        if (parts.length != 2) {
-            throw new IllegalArgumentException("corrupt date format");
-        }
-      int asInt = Integer.parseInt(parts[1]);
-      asInt += 100 * Integer.parseInt(parts[0]);
-      res.add(asInt);
-    }
-    return res;
-  }
-
-  public static int mapDateToInteger(String string) {
-      if (string == null) {
-          throw new NullPointerException();
-      }
-    List<String> ls = new ArrayList<>();
-    ls.add(string);
-    List<Integer> res = mapDateToInteger(ls);
-    return res.get(0);
-  }
-
-  public static StringConverter<Number> getStringConverter() {
-    // uses the format dd.mm.yy
-    return new StringConverter<>() {
-      @Override
-      public String toString(Number number) {
-          if (number == null) {
-              throw new NullPointerException();
-          }
-          if (number.intValue() > 3112) {
-              throw new IllegalArgumentException();
-          }
-
-        String nbrStr = Integer.toString(number.intValue());
-          if (nbrStr.length() == 3) {
-              nbrStr = "0" + nbrStr;
-          } else if (nbrStr.length() != 4) {
-              return nbrStr;
-          }
-        return nbrStr.substring(0, 2) + "." + nbrStr.substring(2, 4);
-      }
-
-      @Override
-      public Number fromString(String s) {
-        throw new UnsupportedOperationException();
-      }
-    };
-  }
 
   public static StringConverter<Number> convertFromInstant() {
     return new StringConverter<Number>() {
@@ -91,5 +23,4 @@ public abstract class Utility {
       }
     };
   }
-
 }
