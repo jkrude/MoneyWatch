28f21f3e6055b6f338b60d6bf199fc48a2dec966
jkrude <jakob.krude@hotmail.com>
2020-11-15 11:10:51 +0100

    Add test-execution in order to undo any persistence changes.

--- src/main/test/com/jkrude/main/MainTest.java
+++ src/main/test/com/jkrude/main/MainTest.java
@@ -1,12 +1,21 @@
 package com.jkrude.main;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-import org.junit.Before;
-import org.junit.Test;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 
 public class MainTest {
 
+  private final static URL BACKUP = Main.class.getClassLoader().getResource("persBackup.json");
+  private final static URL OVERWRITE = Main.class.getClassLoader().getResource("pers.json");
 
+  public static void main(String[] args) throws IOException, URISyntaxException {
+    Main.main(args);
+    Files.copy(Path.of(BACKUP.toURI()), Path.of(OVERWRITE.toURI()),
+        StandardCopyOption.REPLACE_EXISTING);
+  }
 
 }
