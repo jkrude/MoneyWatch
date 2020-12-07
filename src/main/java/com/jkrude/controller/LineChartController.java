e35daf18c21ee22eef58f8c4ea06d4f512246012
jkrude <jakob.krude@hotmail.com>
2020-12-07 18:28:12 +0100

    Fix tickRateChanges in LineChartController

--- src/main/java/com/jkrude/controller/LineChartController.java
+++ src/main/java/com/jkrude/controller/LineChartController.java
@@ -3,6 +3,7 @@ package com.jkrude.controller;
 import static java.time.temporal.ChronoUnit.DAYS;
 
 import com.jkrude.main.Main;
+import com.jkrude.material.AlertBox;
 import com.jkrude.material.Model;
 import com.jkrude.material.Money;
 import com.jkrude.material.PropertyFilteredList;
@@ -14,7 +15,6 @@ import com.jkrude.transaction.TransactionContainer;
 import java.text.DecimalFormat;
 import java.time.LocalDate;
 import java.util.List;
-import java.util.Map.Entry;
 import java.util.TreeMap;
 import javafx.beans.binding.Bindings;
 import javafx.beans.binding.DoubleBinding;
@@ -33,6 +33,7 @@ import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart.Data;
 import javafx.scene.chart.XYChart.Series;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ContextMenu;
 import javafx.scene.control.MenuItem;
@@ -73,69 +74,112 @@ public class LineChartController extends DataDependingController {
   private LineChart<Number, Number> lineChart;
 
   @FXML
-  private ChoiceBox<TickRate> rangeSelection;
+  private ChoiceBox<TickRate> tickRateChoiceBox;
 
   private NumberAxis xAxis;
 
-  Series<Number, Number> series;
+  private Series<Number, Number> series;
 
-  TreeMap<LocalDate, PropertyFilteredList<ExtendedTransaction>> dateMap;
+  private TreeMap<LocalDate, PropertyFilteredList<ExtendedTransaction>> dateMap;
+
+  // Reflects rangeSelection.getValue but only if the new choice results in reasonable data.
+  private ReadOnlyObjectWrapper<TickRate> selectedTickRate;
+
 
   @Override
   public void prepare() {
+    // Only extended transactions change therefore no external changes need to be monitored.
     if (super.transactions == null) {
       super.transactions = fetchDataWithPossibleDialog();
       setupChart();
     }
-
   }
 
   @FXML
   private void initialize() {
     xAxis = (NumberAxis) lineChart.getXAxis();
-    rangeSelection.getSelectionModel().select(TickRate.TICKS_PER_DAY);
-    rangeSelection.getItems().addAll(TickRate.values());
-    rangeSelection.getSelectionModel().selectedItemProperty().addListener(this::tickRateChange);
+    lineChart.setAnimated(false);
+    selectedTickRate = new ReadOnlyObjectWrapper<>(TickRate.TICKS_PER_DAY);
+    selectedTickRate.addListener(this::tickRateChange);
+    tickRateChoiceBox.getItems().addAll(TickRate.values());
+    tickRateChoiceBox.getSelectionModel().select(selectedTickRate.get());
+    tickRateChoiceBox.getSelectionModel().selectedItemProperty().addListener(
+        this::choiceBoxEventFilter);
   }
+
 
   private void setupChart() {
     assert super.transactions != null;
+    assert selectedTickRate != null;
     series = new Series<>();
     lineChart.getData().clear();
     lineChart.getData().add(series);
-    dateMap = transformToTickRate(super.transactions.getSourceAsDateMap(), TickRate.TICKS_PER_DAY);
-    populateSeriesAndBind();
+    if (selectedTickRate.get() != TickRate.TICKS_PER_DAY) {
+      // triggers choiceBoxEventFilter → tickRateChange → dataMap change
+      tickRateChoiceBox.setValue(TickRate.TICKS_PER_DAY);
+    } else {
+      dateMap = transformToTickRate(super.transactions.getSourceAsDateMap(),
+          selectedTickRate.get());
+      populateSeriesAndBind();
+    }
     setupAxis();
   }
 
+  private void choiceBoxEventFilter(
+      ObservableValue<? extends TickRate> obsVal,
+      TickRate oldValue,
+      TickRate newValue) {
+
+    assert dateMap != null;
+    // Only change if new plot would have more than one datapoint.
+    if (newValue != TickRate.TICKS_PER_DAY
+        && DAYS.between(this.dateMap.firstKey(), this.dateMap.lastKey()) < newValue.asDays) {
+
+      AlertBox.showAlert(
+          "Not enough data",
+          "In the range you selected you be only one data point",
+          "",
+          AlertType.INFORMATION);
+      // triggers this method → triggers tickRateChange → oldValue will be newValue → return
+      tickRateChoiceBox.setValue(oldValue);
+    } else {
+      selectedTickRate.set(newValue);
+    }
+  }
 
   private void tickRateChange(
       ObservableValue<? extends TickRate> obsVal,
       TickRate oldValue,
       TickRate newValue) {
-//FIXME: Neither the axis nor the data points change
-//    if (oldValue != newValue && newValue != null) {
-//      this.xAxis.setTickUnit(newValue.asDays);
-//      this.dateMap = transformToTickRate(transactions.getSourceAsDateMap(), newValue);
-//      populateSeriesAndBind();
-//    }
+
+    if (oldValue != newValue && newValue != null) {
+      if (this.series != null && !this.series.getData().isEmpty()) {
+        this.series.getData()
+            .forEach(d -> d.YValueProperty().unbind()); // cant clear if yValues are bound
+        this.series.getData().clear();
+      }
+
+      this.xAxis.setTickUnit(newValue.asDays);
+      this.dateMap = transformToTickRate(transactions.getSourceAsDateMap(), newValue);
+      populateSeriesAndBind();
+    }
   }
 
   private void setupAxis() {
     assert xAxis != null;
-    assert dateMap != null;
     assert !dateMap.isEmpty();
-    assert rangeSelection != null;
+    assert selectedTickRate != null;
     xAxis.setAutoRanging(false);
     xAxis.setLowerBound(dateMap.firstKey().toEpochDay());
     xAxis.setUpperBound(dateMap.lastKey().toEpochDay());
-    xAxis.setTickUnit(TickRate.TICKS_PER_DAY.asDays);
+    xAxis.setTickUnit(selectedTickRate.get().asDays);
 
     xAxis.setTickLabelFormatter(Utility.convertFromEpochDay());
   }
 
   private void populateSeriesAndBind() {
-    this.series.getData().clear();
+    assert series.getData().isEmpty();
+    // Init with neutral element for addition.
     DoubleBinding previousDP = new DoubleBinding() {
       @Override
       protected double computeValue() {
@@ -162,7 +206,7 @@ public class LineChartController extends DataDependingController {
     dataPoint.setXValue(localDate.toEpochDay());
 
     this.series.getData().add(dataPoint);
-    // These methods need dataPoint::node → only call after it is added to the series
+    // These methods need dataPoint::node → only call after it is added to the series.
     setToolTip(dataPoint);
     setClickListener(dataPoint, extendedTransactions.getBaseList());
     return currentDP;
@@ -170,7 +214,7 @@ public class LineChartController extends DataDependingController {
 
   private void setClickListener(Data<Number, Number> dataPoint,
       ObservableList<ExtendedTransaction> baseList) {
-    // Add click Listener for every day
+    // Show transactions for this date in a pop up.
     dataPoint.getNode().setOnMouseClicked(
         event -> {
           if (event.getButton() == MouseButton.PRIMARY) {
@@ -182,6 +226,7 @@ public class LineChartController extends DataDependingController {
   }
 
   private ContextMenu contextMenuGenerator(TableRow<ExtendedTransaction> row) {
+    // Context menu for the table pop up.
     ContextMenu contextMenu = new ContextMenu();
     MenuItem ignoreTransaction = new MenuItem("Ignore/Activate transaction");
     ignoreTransaction.setOnAction(event -> row.getItem().switchActive());
@@ -191,6 +236,8 @@ public class LineChartController extends DataDependingController {
 
   private void setToolTip(Data<Number, Number> dataPoint) {
     assert dataPoint.getNode() != null; // dataPoint was added to series
+    // Show the total amount at that date.
+    // The amount is bound to the dataPoint.yValue.
     String prefix = Utility.convertFromEpochDay().toString(dataPoint.getXValue()) + "\n" +
         "Total : ";
     StringProperty property = new SimpleStringProperty();
@@ -199,6 +246,7 @@ public class LineChartController extends DataDependingController {
         () -> new DecimalFormat("###.##").format(dataPoint.YValueProperty().get().doubleValue()),
         dataPoint.YValueProperty()));
     property.bind(Bindings.concat(prefix).concat(other));
+
     Tooltip tlp = new Tooltip();
     tlp.textProperty().bind(property);
     tlp.setShowDelay(new Duration(100));
@@ -207,6 +255,7 @@ public class LineChartController extends DataDependingController {
 
 
   private DoubleProperty bindToSumOfList(FilteredList<ExtendedTransaction> list) {
+    // DoubleProperty that reflects the sum of a list of transactions.
     DoubleProperty d = new SimpleDoubleProperty(Money.mapSum(list).getRawAmount().doubleValue());
     list.addListener(new ListChangeListener<ExtendedTransaction>() {
       @Override
@@ -230,17 +279,20 @@ public class LineChartController extends DataDependingController {
       final TreeMap<LocalDate, List<ExtendedTransaction>> dateMap,
       final TickRate tickRate) {
 
+    // Accumulate dates if tickRate != DAYS.
+    // Transactions will be stored in filteredLists, which ignores deactivated transactions.
     TreeMap<LocalDate, PropertyFilteredList<ExtendedTransaction>> accumulatedMap = new TreeMap<>();
     LocalDate currDate = LocalDate.MIN;
-    for (Entry<LocalDate, List<ExtendedTransaction>> entry : dateMap.entrySet()) {
+    for (var entry : dateMap.entrySet()) {
+      // If the last-saved-date is older than the current date → create new data-point
       if (DAYS.between(currDate, entry.getKey()) >= tickRate.asDays) {
-        PropertyFilteredList<ExtendedTransaction> list = new PropertyFilteredList<>(
+        var list = new PropertyFilteredList<>(
             ExtendedTransaction::isActiveProperty, entry.getValue());
         accumulatedMap.put(entry.getKey(), list);
+        currDate = entry.getKey();
       } else {
         accumulatedMap.lastEntry().getValue().addAll(entry.getValue());
       }
-      currDate = entry.getKey();
     }
     return accumulatedMap;
   }
