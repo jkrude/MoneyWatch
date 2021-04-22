jkrude/MoneyWatch

commit 318ad5d6ee1509fa7306a6da1252f8c05376f1f9

Fix no-update on ignore undefined transaction

src/main/java/com/jkrude/controller/SunburstChartViewModel.java
@@ -5,6 +5,7 @@ import com.jkrude.category.CategoryValueNode;
 import com.jkrude.category.CategoryValueTree;
 import com.jkrude.category.TreeNodeAdapter;
 import com.jkrude.material.Model;
+import com.jkrude.material.PropertyFilteredList;
 import com.jkrude.material.Utility;
 import com.jkrude.transaction.ExtendedTransaction;
 import com.jkrude.transaction.TransactionContainer;
@@ -16,11 +17,13 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+import javafx.beans.InvalidationListener;
 import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.value.ChangeListener;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.collections.SetChangeListener;
 import javafx.scene.paint.Color;
 
 public class SunburstChartViewModel implements ViewModel {
@@ -35,12 +38,14 @@ public class SunburstChartViewModel implements ViewModel {
   private ObservableList<ExtendedTransaction> negativeTransactions;
   //private final Cate undefinedSegment;
   private final CategoryValueTree categoryValueTree;
+  private final InvalidationListener isActiveListener;
 
 
   public SunburstChartViewModel() {
     globalModel = Model.getInstance();
     invalidatedProperty = new SimpleBooleanProperty();
     nameToDataMap = new HashMap<>();
+    isActiveListener = (observable) -> invalidatedProperty.set(true);
 
     negativeTransactions =
         hasActiveDataProperty()
@@ -67,7 +72,9 @@ public class SunburstChartViewModel implements ViewModel {
         .asTreeNode(this.categoryValueTree, this.nameToDataMap);
     ChartItem item = new ChartItem();
     item.setName(UNDEFINED_SEGMENT);
-    item.valueProperty().bind(Utility.bindToSumOfSet(categoryValueTree.getUnmatchedTransactions()));
+    PropertyFilteredList<ExtendedTransaction> unmatchedFiltered = new PropertyFilteredList<>(
+        ExtendedTransaction::isActiveProperty, categoryValueTree.getUnmatchedTransactions());
+    item.valueProperty().bind(Utility.bindToSumOfList(unmatchedFiltered));
     item.setFill(Color.GRAY);
     TreeNode<ChartItem> undefinedNode = new TreeNode<>(item, adaptedRoot);
     adaptedRoot.addNode(undefinedNode);
@@ -101,6 +108,19 @@ public class SunburstChartViewModel implements ViewModel {
           onActiveDataChange(globalModel.getActiveData());
           invalidatedProperty.set(true);
         });
+    categoryValueTree.getUnmatchedTransactions().addListener(
+        new SetChangeListener<ExtendedTransaction>() {
+          @Override
+          public void onChanged(Change<? extends ExtendedTransaction> change) {
+            if (change.wasAdded()) {
+              change.getElementAdded().isActiveProperty().addListener(isActiveListener);
+            } else if (change.wasRemoved()) {
+              change.getElementRemoved().isActiveProperty().removeListener(isActiveListener);
+            }
+          }
+        });
+    categoryValueTree.getUnmatchedTransactions()
+        .forEach(t -> t.isActiveProperty().addListener(isActiveListener));
   }
 
 

commit 37ab55124f917b8d6c38ea290d2d1d1f9d985d81

Fixed bug in undefined Segment (introduced in: 318ad5d)

src/main/java/com/jkrude/controller/SunburstChartViewModel.java
@@ -41,6 +41,7 @@ public class SunburstChartViewModel implements ViewModel {
   //private final Cate undefinedSegment;
   private final CategoryValueTree categoryValueTree;
   private final InvalidationListener isActiveListener;
+  private final PropertyFilteredList<ExtendedTransaction> filteredUndefined;
 
 
   public SunburstChartViewModel() {
@@ -48,6 +49,7 @@ public class SunburstChartViewModel implements ViewModel {
     invalidatedProperty = new SimpleBooleanProperty();
     nameToDataMap = new HashMap<>();
     isActiveListener = (observable) -> invalidatedProperty.set(true);
+    filteredUndefined = new PropertyFilteredList<>(ExtendedTransaction::isActiveProperty);
 
     negativeTransactions =
         hasActiveDataProperty()
@@ -74,9 +76,8 @@ public class SunburstChartViewModel implements ViewModel {
         .asTreeNode(this.categoryValueTree, this.nameToDataMap);
     ChartItem item = new ChartItem();
     item.setName(UNDEFINED_SEGMENT);
-    PropertyFilteredList<ExtendedTransaction> unmatchedFiltered = new PropertyFilteredList<>(
-        ExtendedTransaction::isActiveProperty, categoryValueTree.getUnmatchedTransactions());
-    item.valueProperty().bind(Utility.bindToSumOfList(unmatchedFiltered));
+    this.filteredUndefined.setAll(categoryValueTree.getUnmatchedTransactions());
+    item.valueProperty().bind(Utility.bindToSumOfList(this.filteredUndefined));
     item.setFill(Color.GRAY);
     TreeNode<ChartItem> undefinedNode = new TreeNode<>(item, adaptedRoot);
     adaptedRoot.addNode(undefinedNode);
@@ -115,8 +116,10 @@ public class SunburstChartViewModel implements ViewModel {
           @Override
           public void onChanged(Change<? extends ExtendedTransaction> change) {
             if (change.wasAdded()) {
+              filteredUndefined.add(change.getElementAdded());
               change.getElementAdded().isActiveProperty().addListener(isActiveListener);
             } else if (change.wasRemoved()) {
+              filteredUndefined.remove(change.getElementRemoved());
               change.getElementRemoved().isActiveProperty().removeListener(isActiveListener);
             }
           }

src/main/test/com/jkrude/controller/SunburstChartViewModelTest.java
@@ -94,6 +94,19 @@ public class SunburstChartViewModelTest extends ApplicationTest {
   }
 
   @Test
+  public void getAdaptedRootDefineTest() throws ParseException {
+    var adaptedRoot = viewModel.getAdaptedRoot();
+    var undefSegOpt = adaptedRoot.getChildren().stream()
+        .filter(n -> n.getItem().getName().equals(SunburstChartViewModel.UNDEFINED_SEGMENT))
+        .findFirst();
+    assertTrue(undefSegOpt.isPresent());
+    Rule extra = Rule.RuleBuilder.fromPair(new Pair<>(TransactionField.OTHER_PARTY, "Extra"))
+        .build();
+    Model.getInstance().getProfile().getRootCategory().addRule(extra);
+    assertEquals(0d, undefSegOpt.get().getItem().getValue(), 0.001);
+  }
+
+  @Test
   public void possibleActiveDataChange() {
     // Tests isInvalidated and addChangeListener too
     TransactionContainer container = new TransactionContainer();

src/main/test/com/jkrude/material/TestData.java
@@ -15,9 +15,6 @@ import javafx.util.Pair;
 
 public class TestData {
 
-  private static Profile profile;
-
-
   public static TransactionContainer getNewCamtWithTestData() {
     ObservableList<Transaction> entries = FXCollections.observableArrayList();
     entries.add(new Transaction(
@@ -149,10 +146,6 @@ public class TestData {
 
 
   public static Profile getProfile() {
-    if (TestData.profile != null) {
-      return TestData.profile;
-    }
-    TestData.profile = new Profile();
     CategoryNode root = new CategoryNode("debits");
     CategoryNode joy = new CategoryNode("Joy");
     CategoryNode necessaries = new CategoryNode("Necessaries");
@@ -192,6 +185,7 @@ public class TestData {
     } catch (ParseException e) {
       e.printStackTrace();
     }
+    Profile profile = new Profile();
     profile.setRootCategory(root);
     return profile;
   }