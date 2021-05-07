8b9f695e975cc0f322bb2f633ff2756aed185f4e
jkrude <jakob.krude@hotmail.com>
2021-05-07 16:41:37 +0200

    Remove unnecessary persBackup.json

--- src/main/test/com/jkrude/main/MainTest.java
+++ src/main/test/com/jkrude/main/MainTest.java
@@ -11,17 +11,20 @@ import java.nio.file.StandardCopyOption;
 
 public class MainTest {
 
-  private final static URL BACKUP = Main.class.getClassLoader().getResource("persBackup.json");
-  private final static URL OVERWRITE = Main.class.getClassLoader().getResource("pers.json");
+  private final static URL PERSISTENCE = Main.class.getClassLoader().getResource("pers.json");
 
   public static void main(String[] args) throws IOException, URISyntaxException {
+    assert PERSISTENCE != null;
+    Path backupFile = Files.createTempFile(null, ".json");
+    System.out.println(backupFile.toAbsolutePath());
+    backupFile.toFile().deleteOnExit();
+    Files.copy(Path.of(PERSISTENCE.toURI()), backupFile, StandardCopyOption.REPLACE_EXISTING);
     var testData = TestData.getNewCamtWithTestData();
     testData.addExtendedTransaction(TestData.getExtraTransaction());
     Model.getInstance().getTransactionContainerList().add(testData);
     Model.getInstance().setActiveData(testData);
     Main.main(args);
-    Files.copy(Path.of(BACKUP.toURI()), Path.of(OVERWRITE.toURI()),
-        StandardCopyOption.REPLACE_EXISTING);
+    Files.copy(backupFile, Path.of(PERSISTENCE.toURI()), StandardCopyOption.REPLACE_EXISTING);
   }
 
 }
