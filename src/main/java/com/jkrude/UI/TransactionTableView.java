e616564f68035ef9632aad8a2add53c0d4fa289c
jkrude <jakob.krude@hotmail.com>
2021-06-24 14:02:14 +0200

    Clean up TransactionTableView

--- src/main/java/com/jkrude/UI/TransactionTableView.java
+++ src/main/java/com/jkrude/UI/TransactionTableView.java
@@ -3,16 +3,13 @@ package com.jkrude.UI;
 import com.jkrude.material.Money;
 import com.jkrude.material.Utility;
 import com.jkrude.transaction.ExtendedTransaction;
+import com.jkrude.transaction.Transaction.TransactionField;
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.beans.InvalidationListener;
-import javafx.beans.Observable;
 import javafx.beans.binding.Bindings;
 import javafx.beans.property.ObjectProperty;
-import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.SimpleStringProperty;
-import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -29,91 +26,35 @@ import javafx.util.Callback;
 public class TransactionTableView implements Initializable {
 
 
-  @FXML
-  protected TableView<ExtendedTransaction> table;
+  @FXML protected TableView<ExtendedTransaction> table;
   // Columns
-  @FXML
-  protected TableColumn<ExtendedTransaction, String> isActive;
-  @FXML
-  protected TableColumn<ExtendedTransaction, String> accountIban;
-  @FXML
-  protected TableColumn<ExtendedTransaction, String> transferDate;
-  @FXML
-  protected TableColumn<ExtendedTransaction, String> validationDate;
-  @FXML
-  protected TableColumn<ExtendedTransaction, String> transferSpecification;
-  @FXML
-  protected TableColumn<ExtendedTransaction, String> usage;
-  @FXML
-  protected TableColumn<ExtendedTransaction, String> creditorId;
-  @FXML
-  protected TableColumn<ExtendedTransaction, String> mandateReference;
-  @FXML
-  protected TableColumn<ExtendedTransaction, String> customerReferenceRndToEnd;
-  @FXML
-  protected TableColumn<ExtendedTransaction, String> collectionReference;
-  @FXML
-  protected TableColumn<ExtendedTransaction, String> debitOriginalAmount;
-  @FXML
-  protected TableColumn<ExtendedTransaction, String> backDebit;
-  @FXML
-  protected TableColumn<ExtendedTransaction, String> otherParty;
-  @FXML
-  protected TableColumn<ExtendedTransaction, String> iban;
-  @FXML
-  protected TableColumn<ExtendedTransaction, String> bic;
-  @FXML
-  protected TableColumn<ExtendedTransaction, Money> amount;
-  @FXML
-  protected TableColumn<ExtendedTransaction, String> info;
+  @FXML protected TableColumn<ExtendedTransaction, String> isActive;
+  @FXML protected TableColumn<ExtendedTransaction, String> accountIban;
+  @FXML protected TableColumn<ExtendedTransaction, String> transferDate;
+  @FXML protected TableColumn<ExtendedTransaction, String> validationDate;
+  @FXML protected TableColumn<ExtendedTransaction, String> transferSpecification;
+  @FXML protected TableColumn<ExtendedTransaction, String> usage;
+  @FXML protected TableColumn<ExtendedTransaction, String> creditorId;
+  @FXML protected TableColumn<ExtendedTransaction, String> mandateReference;
+  @FXML protected TableColumn<ExtendedTransaction, String> customerReferenceRndToEnd;
+  @FXML protected TableColumn<ExtendedTransaction, String> collectionReference;
+  @FXML protected TableColumn<ExtendedTransaction, String> debitOriginalAmount;
+  @FXML protected TableColumn<ExtendedTransaction, String> backDebit;
+  @FXML protected TableColumn<ExtendedTransaction, String> otherParty;
+  @FXML protected TableColumn<ExtendedTransaction, String> iban;
+  @FXML protected TableColumn<ExtendedTransaction, String> bic;
+  @FXML protected TableColumn<ExtendedTransaction, Money> amount;
+  @FXML protected TableColumn<ExtendedTransaction, String> info;
 
-  private final SimpleIntegerProperty numberOfActivatedColumns;
-  private final ObservableList<TableColumn<ExtendedTransaction, ?>> columnList;
-  protected SimpleIntegerProperty columnWidth;
-
-  public TransactionTableView() {
-    this.numberOfActivatedColumns = new SimpleIntegerProperty();
-    this.columnList = FXCollections
-        .observableArrayList(column -> new Observable[]{column.visibleProperty()});
-  }
 
   @Override
   public void initialize(URL url, ResourceBundle resourceBundle) {
     // Use all available space evenly across the columns
 
-    this.columnList.setAll(
-        isActive,
-        accountIban,
-        transferDate,
-        validationDate,
-        transferSpecification,
-        usage,
-        creditorId,
-        mandateReference,
-        customerReferenceRndToEnd,
-        collectionReference,
-        debitOriginalAmount,
-        backDebit,
-        otherParty,
-        iban,
-        bic,
-        amount,
-        info
-    );
-    numberOfActivatedColumns.set(
-        (int) columnList.stream().filter(column -> column.visibleProperty().get()).count());
-
-    this.columnList.addListener((InvalidationListener) obs -> numberOfActivatedColumns.set(
-        (int) columnList.stream().filter(column -> column.visibleProperty().get()).count()));
-
-    this.columnWidth = new SimpleIntegerProperty();
-    this.columnWidth.bind(table.widthProperty().divide(numberOfActivatedColumns));
-    //table.getColumns().forEach(
-    //    transactionTableColumn -> transactionTableColumn.prefWidthProperty().bind(columnWidth));
     table.setPlaceholder(new Label("No Transactions matching."));
 
     isActive.setCellValueFactory(callback -> {
-          SimpleStringProperty stringProp = new SimpleStringProperty();
+      SimpleStringProperty stringProp = new SimpleStringProperty();
           stringProp.bind(Bindings.when(callback.getValue().isActiveProperty()).then("Active")
               .otherwise("Ignored"));
           return stringProp;
@@ -155,14 +96,6 @@ public class TransactionTableView implements Initializable {
 
     amount.setCellFactory(getColoredCellFactory());
 
-    // Make column visibility optional.
-    for (var column : columnList) {
-      MenuItem item = new MenuItem("Disable");
-      item.setOnAction(action -> column.visibleProperty().set(false));
-      column.setContextMenu(new ContextMenu(item));
-    }
-
-
     table.setRowFactory(
         extendedTransactionTableView -> {
           final TableRow<ExtendedTransaction> row = new TableRow<>();
@@ -174,6 +107,17 @@ public class TransactionTableView implements Initializable {
         }
     );
 
+  }
+
+  // Control which columns should be shown initially.
+  public void setVisible(boolean isActive, TransactionField... fields) {
+    this.isActive.setVisible(isActive);
+    for (var field : TransactionField.values()) {
+      getColumn(field).setVisible(false);
+    }
+    for (var visible : fields) {
+      getColumn(visible).setVisible(true);
+    }
   }
 
   private Callback<TableColumn<ExtendedTransaction, Money>, TableCell<ExtendedTransaction, Money>> getColoredCellFactory() {
@@ -231,4 +175,44 @@ public class TransactionTableView implements Initializable {
   public ObjectProperty<ObservableList<ExtendedTransaction>> itemsProperty() {
     return table.itemsProperty();
   }
+
+  private TableColumn<ExtendedTransaction, ?> getColumn(TransactionField field) {
+    switch (field) {
+
+      case ACCOUNT_IBAN:
+        return this.accountIban;
+      case TRANSFER_DATE:
+        return this.transferDate;
+      case VALIDATION_DATE:
+        return this.validationDate;
+      case TRANSFER_SPECIFICATION:
+        return this.transferSpecification;
+      case USAGE:
+        return this.usage;
+      case CREDITOR_ID:
+        return this.creditorId;
+      case MANDATE_REFERENCE:
+        return this.mandateReference;
+      case CUSTOMER_REFERENCE_END_TO_END:
+        return this.customerReferenceRndToEnd;
+      case COLLECTION_REFERENCE:
+        return this.collectionReference;
+      case DEBIT_ORIGINAL_AMOUNT:
+        return this.debitOriginalAmount;
+      case BACK_DEBIT:
+        return this.backDebit;
+      case OTHER_PARTY:
+        return this.otherParty;
+      case IBAN:
+        return this.iban;
+      case BIC:
+        return this.bic;
+      case AMOUNT:
+        return this.amount;
+      case INFO:
+        return this.info;
+    }
+    throw new IllegalArgumentException("Passed unknown field: " + field.name());
+  }
+
 }
