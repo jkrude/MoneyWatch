7a68477e99bb81fc25c4a1937cb17feaa3a5fbbc
jkrude <jakob.krude@hotmail.com>
2021-06-26 23:22:55 +0200

    Fix NavigationRail bugs with no active data

--- src/main/java/com/jkrude/UI/NavigationRail.java
+++ src/main/java/com/jkrude/UI/NavigationRail.java
@@ -3,17 +3,50 @@ package com.jkrude.UI;
 import com.jfoenix.controls.JFXButton;
 import com.jkrude.main.Main;
 import com.jkrude.main.Main.UsableScene;
+import com.jkrude.material.Model;
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 
-public class NavigationRail {
+public class NavigationRail implements Initializable {
 
 
-  @FXML private JFXButton sunburst;
-  @FXML private JFXButton timeline;
-  @FXML private JFXButton categoryEditor;
-  @FXML private JFXButton data;
+  @FXML
+  private JFXButton sunburst;
+  @FXML
+  private JFXButton timeline;
+  @FXML
+  private JFXButton categoryEditor;
+  @FXML
+  private JFXButton data;
   private UsableScene current;
+
+  @Override
+  public void initialize(URL location, ResourceBundle resources) {
+    // Active charts if data was set.
+    Model.getInstance().activeDataProperty().addListener(
+        (observableValue, oldV, newV) -> {
+          if (newV == null) {
+            sunburst.setDisable(true);
+            timeline.setDisable(true);
+          } else {
+            // Only activate if not already shown.
+            if (current != UsableScene.SUNBURST) {
+              sunburst.setDisable(false);
+            }
+            if (current != UsableScene.TIMELINE) {
+              timeline.setDisable(false);
+            }
+          }
+        });
+    // Initially for data.
+    if (Model.getInstance().activeDataProperty().isNull().get()) {
+      sunburst.setDisable(true);
+      timeline.setDisable(true);
+    }
+  }
 
   public void setCurrent(UsableScene next) {
     if (current != null) {
